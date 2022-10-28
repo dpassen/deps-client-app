6fd6611c7a6c209562028f5f8bab73e32dad26ac
Derek Passen <dpassen1@gmail.com>
2022-10-28 16:58:35 -0500

    Bump node version

--- src/org/passen/deps_client_app/root/karma.conf.js
+++ src/org/passen/deps_client_app/root/karma.conf.js
@@ -1,21 +1,20 @@
 module.exports = function (config) {
-    config.set({
-        browsers: ['jsdom'],
-        reporters: ['progress'],
-        basePath: 'target',
-        files: ['ci.js'],
-        frameworks: ['cljs-test'],
-        plugins: [
-            'karma-cljs-test',
-            'karma-jsdom-launcher'
-        ],
-        colors: true,
-        logLevel: config.LOG_INFO,
-        client: {
-            args: ["shadow.test.karma.init"],
-            singleRun: true
-        },
-        browserDisconnectTimeout: 300000,
-        browserNoActivityTimeout: 600000
-    });
+  config.set({
+    listenAddress: "::",
+    hostname: "127.0.0.1",
+    browsers: ["jsdom"],
+    reporters: ["progress"],
+    basePath: "target",
+    files: ["ci.js"],
+    frameworks: ["cljs-test"],
+    plugins: ["karma-cljs-test", "karma-jsdom-launcher"],
+    colors: true,
+    logLevel: config.LOG_INFO,
+    client: {
+      args: ["shadow.test.karma.init"],
+      singleRun: true,
+    },
+    browserDisconnectTimeout: 300000,
+    browserNoActivityTimeout: 600000,
+  });
 };
