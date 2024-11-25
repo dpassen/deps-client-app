fd8d87e165446c07ad8ec0d04d1781e657cf5f76
Derek Passen <dpassen1@gmail.com>
2024-11-24 19:24:31 -0600

    Revert "Integrate with cljs-react-devtools"
    This reverts commit b6bb9c96a88994ccc7f7706fc7a861b90f8d0125.

--- resources/org/passen/deps_client_app/root/dev/preload.cljs
+++
@@ -1,7 +0,0 @@
-(ns preload
-  (:require
-   [cljs-react-devtools.core :as cljs-react-devtools]))
-
-(cljs-react-devtools/init!
- {:root     (js/document.getElementById "{{artifact/id}}")
-  :shortcut "Control-Shift-Meta-R"})
