c11a4e1e24b0cdb80526d60d84545c4801506906
Derek Passen <dpassen1@gmail.com>
2025-03-18 22:25:57 -0500

    Fix postcss indentation

--- resources/org/passen/deps_client_app/root/postcss.config.js
+++ resources/org/passen/deps_client_app/root/postcss.config.js
@@ -1,12 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 
 module.exports = {
-    plugins: {
-        "@tailwindcss/postcss": {},
-        "postcss-prune-var": {},
-        autoprefixer: {},
-        cssnano: {
-            preset: "default",
-        },
+  plugins: {
+    "@tailwindcss/postcss": {},
+    "postcss-prune-var": {},
+    autoprefixer: {},
+    cssnano: {
+      preset: "default",
     },
+  },
 };
